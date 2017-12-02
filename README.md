0c70f7cf3221a141bbb57f85cefbfefb0c7c190c
Nick Gray <f0rkz@f0rkznet.net>
2017-12-01 23:22:00 -0500

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# chef-sourcemod
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: chef-sourcemod
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
